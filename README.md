d4126b86681d56ca1a7c84800777adb870a3c8f0
Juan Alzate <juandalzatem@gmail.com>
2021-10-07 20:50:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SCPGame
+
+Developed with Unreal Engine 4
---
+++ Source/SCPGame/SCPGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SCPGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCPGAME_API ASCPGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
